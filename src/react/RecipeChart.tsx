500de6f9da9ffb1aa1825380c29a1c0d59cc276b
Adam Thompson <adam.m.thompson@icloud.com>
2024-01-15 13:43:08 -0500

    add jest. minor cleanup of react

--- src/react/RecipeChart.tsx
+++ src/react/RecipeChart.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { Recipe, RecipeComponentProps } from "../types"
+import React from "react";
+import { Recipe, RecipeComponentProps } from "../types";
 import jss from "jss";
-import preset from 'jss-preset-default'
-import { parseRecipe } from '../parser';
-import { RecipeNode } from './RecipeNode';
-import { styles }  from './styles';
-import { useRecipe } from './useRecipe';
+import preset from "jss-preset-default";
+import { parseRecipe } from "../parser";
+import { RecipeNode } from "./RecipeNode";
+import { styles } from "./styles";
+import { useRecipe } from "./useRecipe";
 
-jss.setup(preset())
+jss.setup(preset());
 
-export const RecipeChart = (props: RecipeComponentProps ) => {
-
+export const RecipeChart = (props: RecipeComponentProps) => {
   const { classes } = jss.createStyleSheet(styles(0)).attach();
 
-  console.log(props);
-  const recipe = useRecipe(props)
-  
+  const recipe = useRecipe(props);
+
   return (
     <React.Fragment>
       {recipe && (
@@ -29,5 +27,5 @@ export const RecipeChart = (props: RecipeComponentProps ) => {
         />
       )}
     </React.Fragment>
-  )
-}
+  );
+};
--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,45 @@
+import { has, isNull, isUndefined } from "lodash";
+
 export type Step = {
   id: number;
   verb: string;
   ingredients: string[];
-}
+};
 
 export type TreeNode = {
   id: number;
   verb: string;
   ingredients: string[];
   tree: TreeNode[];
-  depth?: number
-}
+  depth?: number;
+};
 
 export type Recipe = {
-  title: string,
-  original: string,
-  steps: Array<Step>,
-  tree: TreeNode,
-  depth: number,
-  ingredientsList: Array<string>,
-}
+  title: string;
+  original: string;
+  steps: Array<Step>;
+  tree: TreeNode;
+  depth: number;
+  ingredientsList: Array<string>;
+};
+
+export const isRecipe = (maybeRecipe: any) => {
+  return (
+    !isUndefined(maybeRecipe) &&
+    !isNull(maybeRecipe) &&
+    typeof maybeRecipe === "object" &&
+    has(maybeRecipe, "title") &&
+    has(maybeRecipe, "original") &&
+    has(maybeRecipe, "steps") &&
+    has(maybeRecipe, "tree") &&
+    has(maybeRecipe, "depth") &&
+    has(maybeRecipe, "ingredientsList")
+  );
+};
 
 export type RecipeComponentProps = {
-  string?: string,
-  children?: React.ReactChild,
-  json?: Recipe,
-  recipe?: Recipe,
-}
+  string?: string;
+  children?: React.ReactChild;
+  json?: Recipe;
+  recipe?: Recipe;
+};
